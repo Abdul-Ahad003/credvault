c98ea8eef66b8593cb57f19c2d1e6f912f0cc0d6
Abdul-Ahad003 <154303508+Abdul-Ahad003@users.noreply.github.com>
2025-05-04 22:07:14 +0530

    M

--- src/app/assests/components/Navbar.jsx
+++ src/app/assests/components/Navbar.jsx
@@ -3,7 +3,6 @@ import Logo from './Logo'
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import Avatar from 'react-avatar';
 
 
 const Navbar = ( {setLoading} ) => {
@@ -88,8 +87,7 @@ const Navbar = ( {setLoading} ) => {
         {isLoggedIn ? <>
           <div className=' flex justify-between items-center gap-6'>
 
-            <div className=' md:block hidden cursor-pointer'><Avatar name={user ? user.username : ''} size="40" round="50%" /></div>
-            <div className=' md:hidden block cursor-pointe'><Avatar name={user ? user.username : ''} size="35" round="50%" /></div>
+            <div className=' cursor-pointe'> <span className=' md:px-3.5 md:py-3 px-2.5 py-2 text-center font-bold  bg-[#099afc] text-white rounded-full border-0 ' >{user ? user.username[0] : '?'} </span></div>
 
             <button onClick={logout} className=' md:block hidden  cursor-pointer py-1.5 px-3 bg-[#cf0909] text-white rounded-lg border-0 transition-all hover:bg-[#ff2222] font-abee'>Logout</button>
             <button onClick={logout} className=' md:hidden block p-1  bg-[#cf0909] text-white rounded-full border-0 transition-all hover:bg-red-600 font-abee'><img src='../logout.svg' alt='log' className=' w-6 h-6' /></button>
